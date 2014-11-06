d8baabeb36b49ab56e2c7b6a44fb0f068cb753e8
Nikhil S <nikhil.sonnad@gmail.com>
2014-11-05 23:51:34 -0500

    add some notes to lesson 1

--- 01-intro/lesson.hs
+++ 01-intro/lesson.hs
@@ -1,7 +1,9 @@
+-- basic functions
 sumtorial :: Integer -> Integer
 sumtorial 0 = 0
 sumtorial n = n + sumtorial (n - 1)
 
+-- pattern matching on arbitrary boolean expressions with guards
 foo :: Integer -> Integer
 foo 0 = 16
 foo 1
@@ -12,26 +14,27 @@ foo n
   | n `mod` 17 == 2 = -43
   | otherwise = n + 3
 
-isEven :: Integer -> Bool
-isEven n = n `mod` 2 == 0
-
 hailstone :: Integer -> Integer
 hailstone n
   | n `mod` 2 == 0 = n `div` 2
-  | otherwise      = 3*n + 1
+  | otherwise      = 3 * n + 1
 
+-- use cons (:) to generate sequence of hailstone interations
 hailstoneSeq :: Integer -> [Integer]
 hailstoneSeq 1 = []
 hailstoneSeq n = n : hailstoneSeq (hailstone n)
 
+-- use _ since we don't need x
 intListLength :: [Integer] -> Integer
 intListLength [] = 0
 intListLength (_:xs) = 1 + intListLength xs
 
+-- patterns can be nested
 sumEveryTwo :: [Integer] -> [Integer]
 sumEveryTwo [] = []
 sumEveryTwo (x:[]) = [x]
 sumEveryTwo (x:(y:zs)) = (x + y) : sumEveryTwo zs
 
+-- this is why it's called functional programming
 hailstoneLen :: Integer -> Integer
 hailstoneLen n = intListLength (hailstoneSeq n) - 1
